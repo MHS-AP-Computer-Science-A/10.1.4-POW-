418da6f3a9b4f1aaaf2515731b5272b2d3ce153b
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-04-06 15:29:33 -0400

    Add files via upload

---
+++ src/Pow.java
@@ -0,0 +1,12 @@
+
+public class Pow {
+	public static void main(String[] args) {
+		
+		System.out.prinln(pow(2, 4)); //Should output 16
+
+	}
+	
+	public static int pow(int base, int n) {
+		
+	}
+}
